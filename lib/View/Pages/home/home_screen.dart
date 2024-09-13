592f5907aa1244ade04918a4447effde7b9fec76
AJi46 <ajilesh46@gmail.com>
2024-09-13 13:56:43 +0530

    added the simmer in home screen

--- lib/View/Pages/home/home_screen.dart
+++ lib/View/Pages/home/home_screen.dart
@@ -1,10 +1,7 @@
-
-import 'dart:async';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quick_o_deals/Controller/Provider/like_button.dart';
+import 'package:quick_o_deals/View/widget/home_widgets/ad_section_page.dart'; // Import the new page
 import 'package:quick_o_deals/View/widget/home_widgets/category_layout.dart';
 import 'package:quick_o_deals/View/widget/home_widgets/horizondal_product.dart';
 
@@ -12,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => LikedHiveProvider (),
+      create: (_) => LikedHiveProvider(),
       child: Scaffold(
         body: SafeArea(
           child: SingleChildScrollView(
@@ -32,7 +29,7 @@ class HomeScreen extends StatelessWidget {
                   _sectionTitle('Featured'),
                   HorizontalProductList(),
                   const SizedBox(height: 20),
-                  _adSection(),
+                  AdSectionPage(), // Use the new page here
                   const SizedBox(height: 20),
                   _sectionTitle('Most Viewed'),
                   HorizontalProductList(),
@@ -48,7 +45,6 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-
   Widget _sectionTitle(String title) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -56,96 +52,6 @@ class HomeScreen extends StatelessWidget {
         Text(title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
         Text('See more', style: TextStyle(color: Colors.blue)),
       ],
-    );
-  }
-
-  Widget _adSection() {
-     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('user_products').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(child: Text("No Ads Available"));
-        }
-
-        final ads = snapshot.data!.docs;
-        final PageController pageController = PageController();
-        Timer.periodic(Duration(seconds: 3), (Timer timer) {
-          if (pageController.hasClients) {
-            int nextPage = pageController.page!.toInt() + 1;
-            if (nextPage >= ads.length) {
-              nextPage = 0;
-            }
-            pageController.animateToPage(
-              nextPage,
-              duration: Duration(milliseconds: 300),
-              curve: Curves.easeIn,
-            );
-          }
-        });
-
-        return SizedBox(
-          height: 200,
-          child: PageView.builder(
-            controller: pageController,
-            itemCount: ads.length,
-            itemBuilder: (context, index) {
-              var adData = ads[index].data() as Map<String, dynamic>;
-              String productName = adData['productName'];
-              String productPrice = adData['productPrice'];
-              List<dynamic> images = adData['images'];
-              String imageUrl = images.isNotEmpty ? images[0] : 'assets/placeholder.png';
-
-              return Stack(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: imageUrl.startsWith('http')
-                            ? NetworkImage(imageUrl)
-                            : AssetImage(imageUrl) as ImageProvider,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 20,
-                    left: 20,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          productName,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 18,
-                            shadows: [Shadow(blurRadius: 10, color: Colors.black)],
-                          ),
-                        ),
-                        Text(
-                          'Rs $productPrice',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            shadows: [Shadow(blurRadius: 10, color: Colors.black)],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        );
-      },
     );
   }
 }
--- lib/View/widget/home_widgets/horizondal_product.dart
+++ lib/View/widget/home_widgets/horizondal_product.dart
@@ -1,133 +1,60 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:quick_o_deals/Controller/Provider/like_button.dart';
-
-// class HorizontalProductList extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<QuerySnapshot>(
-//       stream: FirebaseFirestore.instance.collection('user_products').snapshots(),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return Center(child: CircularProgressIndicator());
-//         }
-
-//         if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-//           return Center(child: Text("No Products Available"));
-//         }
-
-//         final products = snapshot.data!.docs;
-
-//         return SizedBox(
-//           height: 200,
-//           child: ListView.builder(
-//             scrollDirection: Axis.horizontal,
-//             itemCount: products.length,
-//             itemBuilder: (context, index) {
-//               var productData = products[index].data() as Map<String, dynamic>;
-//               String productId = products[index].id;
-//               String productName = productData['productName'];
-//               String productPrice = productData['productPrice'];
-//               String productDetails = productData['productDetails'];
-//               String productAdditionalInfo = productData['productAdditionalInfo'];
-//               List<dynamic> images = productData['images'];
-//               String imageUrl = images.isNotEmpty ? images[0] : 'assets/placeholder.png';
-
-//               return Consumer<LikedHiveProvider>(
-//                 builder: (context, likedProductsProvider, child) {
-//                   bool isLiked = likedProductsProvider.isProductLiked(productId);
-
-//                   return Container(
-//                     width: 150,
-//                     margin: const EdgeInsets.only(right: 10),
-//                     child: Stack(
-//                       children: [
-//                         Column(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             Container(
-//                               height: 100,
-//                               decoration: BoxDecoration(
-//                                 color: Colors.grey[300],
-//                                 borderRadius: BorderRadius.circular(10),
-//                                 image: DecorationImage(
-//                                   image: imageUrl.startsWith('http')
-//                                       ? NetworkImage(imageUrl)
-//                                       : AssetImage(imageUrl) as ImageProvider,
-//                                   fit: BoxFit.cover,
-//                                 ),
-//                               ),
-//                             ),
-//                             const SizedBox(height: 5),
-//                             Text(
-//                               productName,
-//                               style: const TextStyle(fontWeight: FontWeight.bold),
-//                               overflow: TextOverflow.ellipsis,
-//                             ),
-//                             Text(
-//                               'Rs $productPrice',
-//                               style: const TextStyle(color: Colors.green),
-//                             ),
-//                             Text(
-//                               productDetails,
-//                               style: const TextStyle(color: Colors.grey),
-//                               overflow: TextOverflow.ellipsis,
-//                             ),
-//                             Text(
-//                               productAdditionalInfo,
-//                               style: const TextStyle(fontSize: 12, color: Colors.grey),
-//                               overflow: TextOverflow.ellipsis,
-//                             ),
-//                           ],
-//                         ),
-//                         Positioned(
-//                           top: 5,
-//                           right: 5,
-//                           child: IconButton(
-//                             icon: Icon(
-//                               isLiked ? Icons.favorite : Icons.favorite_border,
-//                               color: isLiked ? Colors.red : Colors.white,
-//                             ),
-//                             onPressed: () {
-//                               likedProductsProvider.toggleLike(productId);
-//                             },
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   );
-//                 },
-//               );
-//             },
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
-
-
-
-
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quick_o_deals/Controller/Provider/like_button.dart';
 import 'package:quick_o_deals/View/Pages/product_detailes/product_detailes.dart';
-// Import the Product Details Page
+import 'package:shimmer/shimmer.dart';
 
 class HorizontalProductList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('user_products').snapshots(),
+      stream:
+          FirebaseFirestore.instance.collection('user_products').snapshots(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
+          // Shimmer effect while loading data
+          return SizedBox(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 5, // Number of shimmer placeholders
+              itemBuilder: (context, index) {
+                return Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    width: 150,
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 100,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(height: 5),
+                        Container(
+                          height: 10,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(height: 5),
+                        Container(
+                          height: 10,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(height: 5),
+                        Container(
+                          height: 10,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
         }
 
         if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
@@ -147,20 +74,24 @@ class HorizontalProductList extends StatelessWidget {
               String productName = productData['productName'];
               String productPrice = productData['productPrice'];
               String productDetails = productData['productDetails'];
-              String productAdditionalInfo = productData['productAdditionalInfo'];
+              String productAdditionalInfo =
+                  productData['productAdditionalInfo'];
               List<dynamic> images = productData['images'];
-              String imageUrl = images.isNotEmpty ? images[0] : 'assets/placeholder.jpeg';
+              String imageUrl =
+                  images.isNotEmpty ? images[0] : 'assets/placeholder.jpeg';
 
               return Consumer<LikedHiveProvider>(
                 builder: (context, likedProductsProvider, child) {
-                  bool isLiked = likedProductsProvider.isProductLiked(productId);
+                  bool isLiked =
+                      likedProductsProvider.isProductLiked(productId);
 
                   return GestureDetector(
                     onTap: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => ProductDetailsPage(productId: productId),
+                          builder: (context) =>
+                              ProductDetailsPage(productId: productId),
                         ),
                       );
                     },
@@ -173,22 +104,57 @@ class HorizontalProductList extends StatelessWidget {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Container(
-                                height: 100,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey[300],
-                                  borderRadius: BorderRadius.circular(10),
-                                  image: DecorationImage(
-                                    image: imageUrl.startsWith('http')
-                                        ? NetworkImage(imageUrl)
-                                        : AssetImage(imageUrl) as ImageProvider,
-                                    fit: BoxFit.cover,
+                                  height: 100,
+                                  width: 150,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                      image:
+                                          AssetImage('assets/placeholder.png'),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                ),
-                              ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.network(
+                                      imageUrl,
+                                      fit: BoxFit.cover,
+                                      loadingBuilder: (BuildContext context,
+                                          Widget child,
+                                          ImageChunkEvent? loadingProgress) {
+                                        if (loadingProgress == null) {
+                                          return child;
+                                        } else {
+                                          return Center(
+                                            child: CircularProgressIndicator(
+                                              value: loadingProgress
+                                                          .expectedTotalBytes !=
+                                                      null
+                                                  ? loadingProgress
+                                                          .cumulativeBytesLoaded /
+                                                      (loadingProgress
+                                                              .expectedTotalBytes ??
+                                                          1)
+                                                  : null,
+                                            ),
+                                          );
+                                        }
+                                      },
+                                      errorBuilder: (BuildContext context,
+                                          Object error,
+                                          StackTrace? stackTrace) {
+                                        return Center(
+                                          child: Icon(Icons.error),
+                                        );
+                                      },
+                                    ),
+                                  )),
                               const SizedBox(height: 5),
                               Text(
                                 productName,
-                                style: const TextStyle(fontWeight: FontWeight.bold),
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
                                 overflow: TextOverflow.ellipsis,
                               ),
                               Text(
@@ -202,7 +168,8 @@ class HorizontalProductList extends StatelessWidget {
                               ),
                               Text(
                                 productAdditionalInfo,
-                                style: const TextStyle(fontSize: 12, color: Colors.grey),
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.grey),
                                 overflow: TextOverflow.ellipsis,
                               ),
                             ],
@@ -212,7 +179,9 @@ class HorizontalProductList extends StatelessWidget {
                             right: 5,
                             child: IconButton(
                               icon: Icon(
-                                isLiked ? Icons.favorite : Icons.favorite_border,
+                                isLiked
+                                    ? Icons.favorite
+                                    : Icons.favorite_border,
                                 color: isLiked ? Colors.red : Colors.white,
                               ),
                               onPressed: () {
